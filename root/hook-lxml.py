b26628ac23900eea072dc46975c386ac8ba95c2e
z-Vaughan <zavaugha@amazon.com>
2025-03-19 14:47:43 -0400

    Delete root directory
    Prep for update

--- root/hook-lxml.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
-
-hiddenimports = collect_submodules('lxml')
-datas = collect_data_files('lxml')
--- root/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="pick_assist",
-    version="0.1",
-    packages=find_packages(),
-    package_dir={'': 'src'}
-)
